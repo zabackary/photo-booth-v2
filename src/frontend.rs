5be23220c5f2367568f160cb962ae44b96326b17
zabackary <137591653+zabackary@users.noreply.github.com>
2026-02-14 23:10:31 +0900

    refactor: update main to be straightforward

--- src/frontend.rs
+++ src/frontend.rs
@@ -3,3 +3,147 @@ pub mod loading_spinners;
 pub mod main_app;
 pub mod setup;
 pub mod title_overlay;
+
+use std::time::Duration;
+
+use super::backend::manager::BackendManager;
+use iced::{Font, Task, keyboard::Key, theme::Palette};
+use main_app::{MainApp, MainAppMessage};
+use setup::{Setup, SetupMessage};
+
+enum AppPage {
+    Setup(Setup),
+    MainApp(MainApp),
+}
+
+pub struct PhotoBoothApplication {
+    page: AppPage,
+    manager: BackendManager,
+}
+
+#[derive(Debug, Clone)]
+pub enum PhotoBoothMessage {
+    Setup(SetupMessage),
+    MainApp(MainAppMessage),
+    Tick,
+    SpaceReleased,
+    EscapeReleased,
+    UpReleased,
+    DownReleased,
+    OtherKeyRelease,
+}
+
+#[derive(Debug, Clone, Copy)]
+enum KeyMessage {
+    Space,
+    Up,
+    Down,
+    Escape,
+}
+
+impl PhotoBoothApplication {
+    pub fn new(manager: BackendManager) -> Self {
+        Self {
+            page: AppPage::Setup(Setup::new()),
+            manager,
+        }
+    }
+
+    pub fn update(&mut self, message: PhotoBoothMessage) -> Task<PhotoBoothMessage> {
+        match message {
+            PhotoBoothMessage::Setup(msg) => match &mut self.page {
+                AppPage::Setup(page) => {
+                    let update_task = page.update(msg).map(PhotoBoothMessage::Setup);
+                    if let Some(new_page) = page.new_page.take() {
+                        let (new_page, new_task) = *new_page;
+                        self.page = new_page;
+                        update_task.chain(new_task)
+                    } else {
+                        update_task
+                    }
+                }
+                _ => Task::none(),
+            },
+            PhotoBoothMessage::MainApp(msg) => match &mut self.page {
+                AppPage::MainApp(page) => {
+                    let update_task = page
+                        .update(msg, self.server_backend.clone())
+                        .map(PhotoBoothMessage::MainApp);
+                    if let Some(new_page) = page.new_page.take() {
+                        let (new_page, new_task) = *new_page;
+                        self.page = new_page;
+                        update_task.chain(new_task)
+                    } else {
+                        update_task
+                    }
+                }
+                _ => Task::none(),
+            },
+            PhotoBoothMessage::Tick => match &mut self.page {
+                AppPage::MainApp(page) => page
+                    .update(MainAppMessage::Tick, self.server_backend.clone())
+                    .map(PhotoBoothMessage::MainApp),
+                _ => Task::none(),
+            },
+            PhotoBoothMessage::SpaceReleased
+            | PhotoBoothMessage::DownReleased
+            | PhotoBoothMessage::UpReleased
+            | PhotoBoothMessage::EscapeReleased => match &mut self.page {
+                AppPage::MainApp(page) => page
+                    .update(
+                        MainAppMessage::KeyReleased(match message {
+                            PhotoBoothMessage::SpaceReleased => KeyMessage::Space,
+                            PhotoBoothMessage::DownReleased => KeyMessage::Down,
+                            PhotoBoothMessage::UpReleased => KeyMessage::Up,
+                            PhotoBoothMessage::EscapeReleased => KeyMessage::Escape,
+                            _ => unreachable!(),
+                        }),
+                        self.server_backend.clone(),
+                    )
+                    .map(PhotoBoothMessage::MainApp),
+                _ => Task::none(),
+            },
+            PhotoBoothMessage::OtherKeyRelease => match &mut self.page {
+                AppPage::MainApp(page) => page
+                    .update(MainAppMessage::OtherKeyPress, self.server_backend.clone())
+                    .map(PhotoBoothMessage::MainApp),
+                _ => Task::none(),
+            },
+        }
+    }
+
+    pub fn view(&self) -> iced::Element<PhotoBoothMessage> {
+        match &self.page {
+            AppPage::MainApp(page) => page
+                .view(&self.server_backend)
+                .map(PhotoBoothMessage::MainApp),
+            AppPage::Setup(page) => page.view().map(PhotoBoothMessage::Setup),
+        }
+    }
+
+    pub fn subscription(&self) -> iced::Subscription<PhotoBoothMessage> {
+        const FPS: f32 = 30.0;
+        iced::Subscription::batch([
+            iced::time::every(Duration::from_secs_f32(1.0 / FPS))
+                .map(|_tick| PhotoBoothMessage::Tick),
+            iced::keyboard::on_key_press(|key, _modifiers| match key {
+                Key::Named(iced::keyboard::key::Named::Space)
+                | Key::Named(iced::keyboard::key::Named::Enter) => {
+                    Some(PhotoBoothMessage::SpaceReleased)
+                }
+                Key::Named(iced::keyboard::key::Named::Escape) => {
+                    Some(PhotoBoothMessage::EscapeReleased)
+                }
+                Key::Named(iced::keyboard::key::Named::PageUp)
+                | Key::Named(iced::keyboard::key::Named::ArrowUp) => {
+                    Some(PhotoBoothMessage::UpReleased)
+                }
+                Key::Named(iced::keyboard::key::Named::PageDown)
+                | Key::Named(iced::keyboard::key::Named::ArrowDown) => {
+                    Some(PhotoBoothMessage::DownReleased)
+                }
+                _ => Some(PhotoBoothMessage::OtherKeyRelease),
+            }),
+        ])
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,194 +1,63 @@
-use std::time::Duration;
+use anyhow::Context;
+use clap::Parser;
 
-// use backend::{
-//     camera::{CameraBackend, DefaultCameraBackend},
-//     servers::{DefaultServerBackend, ServerBackend},
-// };
-// use frontend::{
-//     main_app::{MainApp, MainAppMessage},
-//     setup::{Setup, SetupMessage},
-// };
-use iced::{Font, Task, keyboard::Key, theme::Palette};
+use crate::frontend::PhotoBoothApplication;
 
 mod backend;
 mod config;
-// mod frontend;
+mod frontend;
 
-// enum AppPage<
-//     C: crate::backend::camera::CameraBackend + 'static,
-//     S: crate::backend::servers::ServerBackend + 'static,
-// > {
-//     Setup(Setup<C, S>),
-//     MainApp(MainApp<C, S>),
-// }
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Path to the configuration file
+    ///
+    /// The configuration file should be in JSON format. Documentation for the
+    /// config file can be found in `src/config.rs`.
+    #[arg(short, long)]
+    config: String,
+}
 
-// struct PhotoBoothApplication<
-//     C: crate::backend::camera::CameraBackend + 'static,
-//     S: crate::backend::servers::ServerBackend + 'static,
-// > {
-//     page: AppPage<C, S>,
-//     server_backend: S,
-// }
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    // Set up logging
+    env_logger::init();
 
-// #[derive(Debug, Clone)]
-// enum PhotoBoothMessage<
-//     C: crate::backend::camera::CameraBackend + 'static,
-//     S: crate::backend::servers::ServerBackend + 'static,
-// > {
-//     Setup(SetupMessage<C>),
-//     MainApp(MainAppMessage<S>),
-//     Tick,
-//     SpaceReleased,
-//     EscapeReleased,
-//     UpReleased,
-//     DownReleased,
-//     OtherKeyRelease,
-// }
+    // Parse the config file
+    log::info!("Parsing config");
+    let args = Args::parse();
+    let config_str = tokio::fs::read_to_string(args.config)
+        .await
+        .with_context(|| format!("failed to read config file at path: {}", args.config))?;
+    let config: config::Config =
+        serde_json::from_str(&config_str).context("failed to parse config")?;
 
-// #[derive(Debug, Clone, Copy)]
-// enum KeyMessage {
-//     Space,
-//     Up,
-//     Down,
-//     Escape,
-// }
+    // Start the backend
+    log::info!("Starting backend");
+    let manager = backend::manager::BackendManager::from_config(&config)
+        .await
+        .context("failed to start backend")?;
 
-// impl<
-//         C: crate::backend::camera::CameraBackend + 'static + Clone,
-//         S: crate::backend::servers::ServerBackend + 'static,
-//     > PhotoBoothApplication<C, S>
-// {
-//     fn update(&mut self, message: PhotoBoothMessage<C, S>) -> Task<PhotoBoothMessage<C, S>> {
-//         match message {
-//             PhotoBoothMessage::Setup(msg) => match &mut self.page {
-//                 AppPage::Setup(page) => {
-//                     let update_task = page.update(msg).map(PhotoBoothMessage::Setup);
-//                     if let Some(new_page) = page.new_page.take() {
-//                         let (new_page, new_task) = *new_page;
-//                         self.page = new_page;
-//                         update_task.chain(new_task)
-//                     } else {
-//                         update_task
-//                     }
-//                 }
-//                 _ => Task::none(),
-//             },
-//             PhotoBoothMessage::MainApp(msg) => match &mut self.page {
-//                 AppPage::MainApp(page) => {
-//                     let update_task = page
-//                         .update(msg, self.server_backend.clone())
-//                         .map(PhotoBoothMessage::MainApp);
-//                     if let Some(new_page) = page.new_page.take() {
-//                         let (new_page, new_task) = *new_page;
-//                         self.page = new_page;
-//                         update_task.chain(new_task)
-//                     } else {
-//                         update_task
-//                     }
-//                 }
-//                 _ => Task::none(),
-//             },
-//             PhotoBoothMessage::Tick => match &mut self.page {
-//                 AppPage::MainApp(page) => page
-//                     .update(MainAppMessage::Tick, self.server_backend.clone())
-//                     .map(PhotoBoothMessage::MainApp),
-//                 _ => Task::none(),
-//             },
-//             PhotoBoothMessage::SpaceReleased
-//             | PhotoBoothMessage::DownReleased
-//             | PhotoBoothMessage::UpReleased
-//             | PhotoBoothMessage::EscapeReleased => match &mut self.page {
-//                 AppPage::MainApp(page) => page
-//                     .update(
-//                         MainAppMessage::KeyReleased(match message {
-//                             PhotoBoothMessage::SpaceReleased => KeyMessage::Space,
-//                             PhotoBoothMessage::DownReleased => KeyMessage::Down,
-//                             PhotoBoothMessage::UpReleased => KeyMessage::Up,
-//                             PhotoBoothMessage::EscapeReleased => KeyMessage::Escape,
-//                             _ => unreachable!(),
-//                         }),
-//                         self.server_backend.clone(),
-//                     )
-//                     .map(PhotoBoothMessage::MainApp),
-//                 _ => Task::none(),
-//             },
-//             PhotoBoothMessage::OtherKeyRelease => match &mut self.page {
-//                 AppPage::MainApp(page) => page
-//                     .update(MainAppMessage::OtherKeyPress, self.server_backend.clone())
-//                     .map(PhotoBoothMessage::MainApp),
-//                 _ => Task::none(),
-//             },
-//         }
-//     }
+    // Start the frontend
+    log::info!("Starting application");
+    iced::application(
+        "Photo Booth",
+        PhotoBoothApplication::update,
+        PhotoBoothApplication::view,
+    )
+    .font(include_bytes!(
+        "../assets/fonts/Noto_Color_Emoji/NotoColorEmoji-Regular.ttf"
+    ))
+    .font(include_bytes!(
+        "../assets/fonts/Montserrat/Montserrat-Regular.ttf"
+    ))
+    .default_font(iced::Font::with_name("Montserrat"))
+    .theme(|_| iced::Theme::custom("Custom palette".to_owned(), config.theme.into()))
+    .subscription(PhotoBoothApplication::subscription)
+    .run_with(|| (PhotoBoothApplication::new(manager), iced::Task::none()))
+    .context("failed to run application");
 
-//     fn view(&self) -> iced::Element<PhotoBoothMessage<C, S>> {
-//         match &self.page {
-//             AppPage::MainApp(page) => page
-//                 .view(&self.server_backend)
-//                 .map(PhotoBoothMessage::MainApp),
-//             AppPage::Setup(page) => page.view().map(PhotoBoothMessage::Setup),
-//         }
-//     }
-
-//     fn subscription(&self) -> iced::Subscription<PhotoBoothMessage<C, S>> {
-//         const FPS: f32 = 30.0;
-//         iced::Subscription::batch([
-//             iced::time::every(Duration::from_secs_f32(1.0 / FPS))
-//                 .map(|_tick| PhotoBoothMessage::Tick),
-//             iced::keyboard::on_key_press(|key, _modifiers| match key {
-//                 Key::Named(iced::keyboard::key::Named::Space)
-//                 | Key::Named(iced::keyboard::key::Named::Enter) => {
-//                     Some(PhotoBoothMessage::SpaceReleased)
-//                 }
-//                 Key::Named(iced::keyboard::key::Named::Escape) => {
-//                     Some(PhotoBoothMessage::EscapeReleased)
-//                 }
-//                 Key::Named(iced::keyboard::key::Named::PageUp)
-//                 | Key::Named(iced::keyboard::key::Named::ArrowUp) => {
-//                     Some(PhotoBoothMessage::UpReleased)
-//                 }
-//                 Key::Named(iced::keyboard::key::Named::PageDown)
-//                 | Key::Named(iced::keyboard::key::Named::ArrowDown) => {
-//                     Some(PhotoBoothMessage::DownReleased)
-//                 }
-//                 _ => Some(PhotoBoothMessage::OtherKeyRelease),
-//             }),
-//         ])
-//     }
-// }
-
-// fn main() -> iced::Result {
-//     // Set up logging
-//     env_logger::init();
-//     log::info!("Starting Photo Booth");
-
-//     type CameraBackend = DefaultCameraBackend;
-//     type ServerBackend = DefaultServerBackend;
-
-//     CameraBackend::initialize().expect("failed to initialize camera backend");
-
-//     iced::application(
-//         "Photo Booth",
-//         PhotoBoothApplication::update,
-//         PhotoBoothApplication::view,
-//     )
-//     .font(include_bytes!(
-//         "../assets/fonts/Noto_Color_Emoji/NotoColorEmoji-Regular.ttf"
-//     ))
-//     .font(include_bytes!(
-//         "../assets/fonts/Montserrat/Montserrat-Regular.ttf"
-//     ))
-//     .default_font(Font::with_name("Montserrat"))
-//     .theme(|_| iced::Theme::custom("CAJ".to_owned(), PALETTE))
-//     .subscription(PhotoBoothApplication::subscription)
-//     .run_with(|| {
-//         let server_backend = ServerBackend::new().expect("failed to initialize server backend");
-//         (
-//             PhotoBoothApplication::<CameraBackend, ServerBackend> {
-//                 page: AppPage::Setup(Setup::new()),
-//                 server_backend,
-//             },
-//             Task::none(),
-//         )
-//     })
-// }
+    log::info!("Application exited successfully");
+    Ok(())
+}
