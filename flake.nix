b06cb5e416ea7c4078cb89b49191d4a2b7e4aa21
zabackary <137591653+zabackary@users.noreply.github.com>
2026-02-17 19:13:26 +0900

    fix: make platform deps be correct

--- flake.nix
+++ flake.nix
@@ -23,32 +23,68 @@
         pkgs = import nixpkgs {
           inherit system overlays;
         };
-        nativeBuildInputs = with pkgs; [
-          pkg-config
-          libclang
-          linuxHeaders
-          glibc.dev
-          libgphoto2
-          mold-wrapped
+
+        isLinux = pkgs.stdenv.isLinux;
+        lib = pkgs.lib;
+
+        # compile-time (native) tools
+        nativeBuildInputs = lib.concatMap (x: x) [
+          (lib.optionals isLinux [
+            pkgs.linuxHeaders
+            pkgs.glibc.dev
+          ])
+          [
+            pkgs.pkg-config
+            pkgs.libclang
+            pkgs.mold-wrapped
+          ]
         ];
-        buildInputs = with pkgs; [
-          openssl
-          libv4l
-        ];
-        libPath =
+
+        # runtime / link libraries (only pulled on Linux)
+        runtimeLibs =
+          if isLinux then
+            with pkgs;
+            [
+              libgphoto2
+              libGL
+              libxkbcommon
+              vulkan-loader
+              wayland
+              xorg.libXcursor
+              xorg.libXrandr
+              xorg.libXi
+              xorg.libX11
+            ]
+          else
+            [ ];
+
+        # buildInputs contains libraries needed to compile/link and at runtime
+        buildInputs =
           with pkgs;
-          lib.makeLibraryPath [
-            # Wayland
-            libGL
-            libxkbcommon
-            vulkan-loader
-            wayland
-            # X11
-            xorg.libXcursor
-            xorg.libXrandr
-            xorg.libXi
-            xorg.libX11
-          ];
+          (
+            [
+              openssl
+              libv4l
+            ]
+            ++ runtimeLibs
+          );
+
+        libPath =
+          if isLinux then
+            with pkgs;
+            lib.makeLibraryPath [
+              libGL
+              libxkbcommon
+              vulkan-loader
+              wayland
+              xorg.libXcursor
+              xorg.libXrandr
+              xorg.libXi
+              xorg.libX11
+            ]
+          else
+            "";
+
         naersk-lib = naersk.lib."${system}";
       in
       {
@@ -67,7 +103,8 @@
           ++ buildInputs;
 
           LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
-          BINDGEN_EXTRA_CLANG_ARGS = "-I${pkgs.linuxHeaders}/include -I${pkgs.glibc.dev}/include";
+          BINDGEN_EXTRA_CLANG_ARGS =
+            if isLinux then "-I${pkgs.linuxHeaders}/include -I${pkgs.glibc.dev}/include" else "";
           LD_LIBRARY_PATH = libPath;
         };
       }
