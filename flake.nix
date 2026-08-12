877cf6fe271740876f908b8e4aec11427416ea70
zabackary <137591653+zabackary@users.noreply.github.com>
2026-08-12 19:30:22 +0900

    refactor: use a simpler nix flake

--- flake.nix
+++ flake.nix
@@ -2,35 +2,29 @@
   description = "Photo Booth V2 - Rust project";
 
   inputs = {
-    flake-utils.url = "github:numtide/flake-utils";
-    naersk.url = "github:nix-community/naersk";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
-    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
   outputs =
-    {
-      self,
-      flake-utils,
-      naersk,
-      nixpkgs,
-      rust-overlay,
-    }:
-    flake-utils.lib.eachSystem
-      [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" "x86_64-pc-windows-msvc" ]
-      (
+    { self, nixpkgs }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      perSystem = nixpkgs.lib.genAttrs systems (
         system:
         let
-          overlays = [ (import rust-overlay) ];
-          pkgs = import nixpkgs {
-            inherit system overlays;
-          };
-
-          toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+          pkgs = import nixpkgs { inherit system; };
 
           isLinux = pkgs.stdenv.isLinux;
           isDarwin = pkgs.stdenv.isDarwin;
           lib = pkgs.lib;
+
+          cargoToml = lib.importTOML ./Cargo.toml;
 
           # compile-time (native) tools
           nativeBuildInputs =
@@ -89,42 +83,62 @@
             else
               "";
 
-          naersk' = pkgs.callPackage naersk {
-            cargo = toolchain;
-            rustc = toolchain;
+          envVars = {
+            LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
+            BINDGEN_EXTRA_CLANG_ARGS =
+              if isLinux then
+                "-I${pkgs.linuxHeaders}/include -I${pkgs.glibc.dev}/include -I${pkgs.cups.dev}/include"
+              else
+                "";
           };
         in
         {
-          packages.default = naersk'.buildPackage {
-            inherit nativeBuildInputs buildInputs;
-            src = ./.;
-            postInstall = ''
-              wrapProgram "$out/bin/photo-booth-v2" --prefix LD_LIBRARY_PATH : ${libPath} --prefix DYLD_LIBRARY_PATH : ${libPath}
-            '';
+          packages.default = pkgs.rustPlatform.buildRustPackage (
+            {
+              pname = cargoToml.package.name;
+              version = cargoToml.package.version;
+              src = ./.;
 
-            LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
-            BINDGEN_EXTRA_CLANG_ARGS =
-              if isLinux then
-                "-I${pkgs.linuxHeaders}/include -I${pkgs.glibc.dev}/include -I${pkgs.cups.dev}/include"
-              else
-                "";
-          };
+              cargoLock = {
+                lockFile = ./Cargo.lock;
+                # nokhwa is pulled from git; every crate vendored from that
+                # same repo+rev shares this one hash.
+                outputHashes = {
+                  "nokhwa-0.10.10" = "sha256-2mrGDmYOQom2UXc+8rJOkurWu4u/JL2/Jx6CA6mqrnU=";
+                };
+              };
 
-          devShells.default = pkgs.mkShell {
-            inherit nativeBuildInputs;
-            buildInputs = [
-              toolchain
-            ]
-            ++ buildInputs;
+              inherit nativeBuildInputs buildInputs;
 
-            LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
-            BINDGEN_EXTRA_CLANG_ARGS =
-              if isLinux then
-                "-I${pkgs.linuxHeaders}/include -I${pkgs.glibc.dev}/include -I${pkgs.cups.dev}/include"
-              else
-                "";
-            LD_LIBRARY_PATH = libPath;
-          };
+              postInstall = ''
+                wrapProgram "$out/bin/${cargoToml.package.name}" --prefix LD_LIBRARY_PATH : ${libPath} --prefix DYLD_LIBRARY_PATH : ${libPath}
+              '';
+            }
+            // envVars
+          );
+
+          devShells.default = pkgs.mkShell (
+            {
+              inherit nativeBuildInputs;
+              buildInputs = [
+                pkgs.cargo
+                pkgs.rustc
+                pkgs.rustfmt
+                pkgs.clippy
+                pkgs.rust-analyzer
+              ]
+              ++ buildInputs;
+
+              RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
+              LD_LIBRARY_PATH = libPath;
+            }
+            // envVars
+          );
         }
       );
+    in
+    {
+      packages = nixpkgs.lib.mapAttrs (_: v: v.packages) perSystem;
+      devShells = nixpkgs.lib.mapAttrs (_: v: v.devShells) perSystem;
+    };
 }
